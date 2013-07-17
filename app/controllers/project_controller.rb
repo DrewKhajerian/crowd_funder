4e2bb55bc1e7b1b153eb55ce26aa3489bf496ef8
DrewKhajerian <arkhajer@gmail.com>
2013-07-17 14:51:55 -0400

    added project index and show views

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,25 @@
+class ProjectController < ApplicationController
+
+	def index
+		@projects = Project.all
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(params[:project])
+
+		if @project.save
+			redirect_to project_path(@project)
+		else
+			render :new
+		end
+	end
+
+end
