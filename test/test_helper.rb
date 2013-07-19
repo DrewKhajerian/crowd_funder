1980d9224799b408f290d088ca61db8d92926b36
DrewKhajerian <arkhajer@gmail.com>
2013-07-19 12:53:10 -0400

    wrote mailer tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,6 +24,12 @@ class ActionDispatch::IntegrationTest
     Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
   end
 
+  self.use_transactional_fixtures = false 
+
+  setup do
+    reset_email
+  end
+
   def setup_signed_in_user
     pass = "this-is-a-password"
     user = FactoryGirl.create :user, password: pass
@@ -34,5 +40,13 @@ class ActionDispatch::IntegrationTest
     click_button "Login"
 
     user
+  end
+
+  def last_email
+    ActionMailer::Base.deliveries.last
+  end
+ 
+  def reset_email
+    ActionMailer::Base.deliveries = []
   end
 end
