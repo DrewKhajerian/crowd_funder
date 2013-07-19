4ce0430a2e19374f1bb1e944f500aabe6b63d340
DrewKhajerian <arkhajer@gmail.com>
2013-07-19 13:16:13 -0400

    write pagination tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,8 @@
 
 # Destroy all pre-existing data before seeding
 
-# User.destroy_all
-# Project.destroy_all
-
-# user = User.create!(first_name: "King", last_name: "Buzzo", email: "king@buzzo.com", password: "foobar")
+User.destroy_all
+Project.destroy_all
 
 # project4 = user.projects.create!(title: "Project 10", teaser: "Teaser text 10",
 #   description: "Description 10", goal: 10000)
@@ -21,4 +19,11 @@
 
 # project6 = user.projects.create!(title: "Project 12", teaser: "Teaser text 12",
 #   description: "Description 12", goal: 50000)
+
+user = User.create!(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "password")
+
+50.times do |i|
+  project1 = user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}",
+    description: "description #{i}", goal: 13000)
+end
 
